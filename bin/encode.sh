3a1aa72255486eced1413ca84c0ce870aa661464
chibiegg <egusa@takenoko-tech.co.jp>
2015-01-31 12:19:17 +0900

    エンコードコマンド (途中)

---
+++ bin/encode.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+WORKER=$1
+URL=$2
+
+
+PROGRAM_ID=
+INPUT_FILE=
+OUTPUT_FILE=
+
+if [ $# -ne 2 ];then
+  echo "Usage: $0 worker_name url"
+  exit 1
+fi
+
+if [ -z "$TVENC_APIKEY" ];then
+  echo "Please set environment 'TVENC_APIKEY'" >&2
+  exit 1
+fi
+
+get_new_program()
+{
+  json=`curl -F "worker=$WORKER" $URL/api/newjob/`
+  if [ -z "$json" ];then
+    return 1
+  fi
+  PROGRAM_ID=`echo "$json" | jq -r .id`
+  INPUT_FILE=`echo "$json" | jq -r .input`
+  OUTPUT_FILE=`echo "$json" | jq -r .output`
+}
+
+
+
+shutdown_handler()
+{
+  if [ -n "$PROGRAM_ID" ];then
+    echo ""
+  fi
+  exit 100
+}
+
+trap shutdown_handler SIGINT
+
+
+
+
+
+
+
+
+while true
+do
+sleep 5
+done
+
+
+
+
+
+
+
+
--- src/tvenc/api/urls.py
+++ src/tvenc/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^newjob.json$', 'tvenc.api.views.get_newjob', name='api_get_newjob'),
-    url(r'^update_status$', 'tvenc.api.views.update_status', name='api_update_status'),
+    url(r'^newjob/$', 'tvenc.api.views.get_newjob', name='api_get_newjob'),
+    url(r'^update_status/$', 'tvenc.api.views.update_status', name='api_update_status'),
 )
 
